5f2d67bc17cac2498ef8832ca7a4e9ac8162615c
James Rezendes <James@MacBook-Pro.local>
2017-09-14 22:52:25 -0700

    Added Results and Payout

--- roulette.py
+++ roulette.py
@@ -15,14 +15,17 @@ redNums = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 
 
 def takeBet():
+    userBetMoney = ''
     userBetChoice = ''
+    while not userBetMoney.isdigit():
+        userBetMoney = raw_input("How much are you going to bet: $")
     while not userBetChoice == "color" and not userBetChoice.isdigit():
-        userBetChoice = raw_input("Make a Bet!\nEnter a number between 0 and 37, or 00 (enter 'color' to just pick a color): ")
+        userBetChoice = raw_input("Enter a number between 0 and 37 (enter 'color' to just pick a color): ")
     if userBetChoice == "color":
         while not userBetChoice == "black" and not userBetChoice == "red" and not userBetChoice == "green":
             userBetChoice = raw_input("Enter a color ('black' , 'red' , 'green'): ")
         userBetChoice = userBetChoice[0].upper() + userBetChoice[1:]
-    return userBetChoice
+    return [userBetChoice, userBetMoney]
 
 
 def roll_ball():
@@ -39,8 +42,18 @@ def roll_ball():
 def checkResults(userBetChoice, ballLandArr):
     loadBar()
     print("Done!")
+    print("========================")
     print("Ball Landed on " + ballLandArr[1] + " " + str(ballLandArr[0]))
-    print("You chose " + userBetChoice)
+    print("You Chose " + userBetChoice[0])
+    print("Your Bet: $" + userBetChoice[1])
+    if userBetChoice[0].isdigit() and int(userBetChoice[0]) == ballLandArr[0]:
+        payout("numWin", userBetChoice[1])
+    elif not userBetChoice[0].isdigit() and userBetChoice[0] == ballLandArr[1]:
+        payout("colorWin", userBetChoice[1])
+    else:
+        payout("lose", userBetChoice[1])
+    print("========================")
+
 
 
 def loadBar():
@@ -56,9 +69,14 @@ def loadBar():
     sys.stdout.write('\x1b[2K')
 
 
-def payout():
-    '''payout for colors vs numbers'''
-    pass
+def payout(caseString, userBetMoney):
+    if caseString == "numWin":
+        print("You Win $" + str((int(userBetMoney)*3)) + "!")
+    if caseString == "colorWin":
+        print("You Win $" + str((int(userBetMoney)*0.50)) + "!")
+    else:
+        print("You Lose $" + userBetMoney)
+
 
 
 def play_game():
